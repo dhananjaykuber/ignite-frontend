0782f204031a4e0318ad9bdbeb267ea26c089109
dhananjaykuber <dnkuber2002@gmail.com>
2023-03-20 01:50:38 +0530

    live minor changes

--- src/pages/Appear.jsx
+++ src/pages/Appear.jsx
@@ -1,140 +1,117 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import styles from '../styles/pages/Events.module.css';
 
 const Appear = () => {
   const navigate = useNavigate();
-  const [bugbounty, setBugBounty] = useState(false);
-  const [aptitude, setAptitude] = useState(false);
-  const [bugbounty2, setBugBounty2] = useState(false);
 
-  useEffect(() => {
-    const bugBounty = async () => {
-      try {
-        try {
-          const response = await axios.get(
-            `${process.env.REACT_APP_NODE_BACKEND}/apinode/category/check-live/bugbounty`
-          );
+  const notifyError = (message) => {
+    toast.error(message, {
+      position: 'top-center',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  };
 
-          setBugBounty(response.data.live);
-          console.log(response.data.live);
-        } catch (error) {
-          console.log(error);
-        }
-      } catch (error) {
-        console.log(error);
+  const bugBounty = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_NODE_BACKEND}/apinode/category/check-live/bugbounty`
+      );
+
+      if (response.data.live) {
+        navigate('/quiz/bugbounty?name=Bug Bounty');
+      } else {
+        notifyError('Bug Bounty Round I is not live yet');
       }
-    };
-    const aptitude = async () => {
-      try {
-        try {
-          const response = await axios.get(
-            `${process.env.REACT_APP_NODE_BACKEND}/apinode/category/check-live/aptitude`
-          );
+      console.log(response.data.live);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const aptitude = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_NODE_BACKEND}/apinode/category/check-live/aptitude`
+      );
 
-          setAptitude(response.data.live);
-        } catch (error) {
-          console.log(error);
-        }
-      } catch (error) {
-        console.log(error);
+      if (response.data.live) {
+        navigate('/quiz/aptitude?name=Mock Placement');
+      } else {
+        notifyError('Aptitude for Mock Placement is not live yet');
       }
-    };
-    const bugBounty2 = async () => {
-      try {
-        try {
-          const response = await axios.get(
-            `${process.env.REACT_APP_NODE_BACKEND}/apinode/category/check-live/bugbounty2`
-          );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const bugBounty2 = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_NODE_BACKEND}/apinode/category/check-live/bugbounty2`
+      );
 
-          setBugBounty2(response.data.live);
-        } catch (error) {
-          console.log(error);
-        }
-      } catch (error) {
-        console.log(error);
+      if (response.data.live) {
+        navigate('/editor/login');
+      } else {
+        notifyError('Bug Bounty Round II is not live yet');
       }
-    };
-
-    bugBounty();
-    aptitude();
-    bugBounty2();
-  }, []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className={styles.events}>
       <div className={styles.allEvents}>
-        {bugbounty && (
-          <div className={styles.event}>
-            <div className={styles.content}>
-              <div className={styles.contentOverlay}></div>
-              <img src="/events/bugbounty.png" alt="bug bounty" />
-              <div
-                className={`${styles.contentDetails} ${styles.contentFadeIn}`}
-              >
-                <h4>
-                  Bug Bounty <br />
-                  Round - I
-                </h4>
-                <button
-                  onClick={() => {
-                    navigate('/quiz/bugbounty?name=Bug Bounty');
-                  }}
-                >
-                  Start Test
-                </button>
-              </div>
+        <div className={styles.event}>
+          <div className={styles.content}>
+            <div className={styles.contentOverlay}></div>
+            <img src="/events/bugbounty.png" alt="bug bounty" />
+            <div className={`${styles.contentDetails} ${styles.contentFadeIn}`}>
+              <h4>
+                Bug Bounty <br />
+                Round - I
+              </h4>
+              <button onClick={bugBounty}>Start Test</button>
             </div>
           </div>
-        )}
-        {aptitude && (
-          <div className={styles.event}>
-            <div className={styles.content}>
-              <div className={styles.contentOverlay}></div>
-              <img src="/events/mockplacement.png" alt="Mock Placement" />
-              <div
-                className={`${styles.contentDetails} ${styles.contentFadeIn}`}
-              >
-                <h4>Mock Placement</h4>
-                <button
-                  onClick={() => {
-                    navigate('/quiz/aptitude?name=Mock Placement');
-                  }}
-                >
-                  Start Test
-                </button>
-              </div>
+        </div>
+
+        <div className={styles.event}>
+          <div className={styles.content}>
+            <div className={styles.contentOverlay}></div>
+            <img src="/events/mockplacement.png" alt="Mock Placement" />
+            <div className={`${styles.contentDetails} ${styles.contentFadeIn}`}>
+              <h4>Mock Placement</h4>
+              <button onClick={aptitude}>Start Test</button>
             </div>
           </div>
-        )}
-        {bugbounty2 && (
-          <div className={styles.event}>
-            <div className={styles.content}>
-              <div className={styles.contentOverlay}></div>
-              <img
-                src="/events/programming.png"
-                alt="Bug Bounty Round II"
-                style={{ width: 150 }}
-              />
-              <div
-                className={`${styles.contentDetails} ${styles.contentFadeIn}`}
-              >
-                <h4>
-                  Bug Bounty <br />
-                  Round - II
-                </h4>
-                <button
-                  onClick={() => {
-                    navigate('/editor/login');
-                  }}
-                >
-                  Start Test
-                </button>
-              </div>
+        </div>
+
+        <div className={styles.event}>
+          <div className={styles.content}>
+            <div className={styles.contentOverlay}></div>
+            <img
+              src="/events/programming.png"
+              alt="Bug Bounty Round II"
+              style={{ width: 150 }}
+            />
+            <div className={`${styles.contentDetails} ${styles.contentFadeIn}`}>
+              <h4>
+                Bug Bounty <br />
+                Round - II
+              </h4>
+              <button onClick={bugBounty2}>Start Test</button>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
