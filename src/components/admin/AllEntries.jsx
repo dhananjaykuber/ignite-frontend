d5937113b229c212185511a16ca39e9e393db174
dhananjaykuber <dnkuber2002@gmail.com>
2023-03-20 01:40:32 +0530

    added token auth

--- src/components/admin/AllEntries.jsx
+++ src/components/admin/AllEntries.jsx
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import convert from 'convert-seconds';
 import styles from '../../styles/pages/Admin.module.css';
 import { useSelector } from 'react-redux';
 
-const options = ['bugbounty', 'aptitude'];
+const options = [
+  'Bug Bounty',
+  'Mock Placement',
+  'Design-X',
+  'Hackathon',
+  'Escape Room',
+];
 
 const AllEntries = () => {
   const { data } = useSelector((store) => store.admin);
 
-  const [state, setState] = useState('bugbounty');
+  const [state, setState] = useState('Bug Bounty');
 
   const [entries, setEntries] = useState([]);
 
   const onEventChange = (e) => {
     setState(e.target.value);
-    console.log(e.target.value);
+    handleLoad(e.target.value);
   };
 
-  const handleLoad = async () => {
+  const handleLoad = async (state) => {
     setEntries([]);
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_NODE_BACKEND}/apinode/quiz/get-entries/${state}`,
+        `${process.env.REACT_APP_FLASK_BACKEND}/api/auth/registration/${state}`,
         {
           headers: {
-            Authorization: `Bearer ${JSON.parse(data).token}`,
+            Authorization: `Bearer ${localStorage.getItem('admin')}`,
           },
         }
       );
@@ -45,33 +50,24 @@ const AllEntries = () => {
             <option key={opt}>{opt}</option>
           ))}
         </select>
-        <button onClick={handleLoad}>Load</button>
+        <button onClick={() => handleLoad(state)}>Load</button>
       </div>
 
       <table border={1}>
         <tr>
+          <th>QR ID</th>
           <th>Name</th>
           <th>Email</th>
           <th>Contact</th>
-          <th>Category</th>
-          <th>Submitted</th>
-          <th>Time</th>
-          <th>Score</th>
         </tr>
         {entries.map((entry) => (
-          <tr key={entry._id}>
-            <td>{entry.name}</td>
+          <tr key={entry.qr_id}>
+            <td>{entry.qr_id}</td>
+            <td>
+              {entry.first_name} {entry.last_name}
+            </td>
             <td>{entry.email}</td>
             <td>{entry.contact}</td>
-            <td>{entry.category}</td>
-            <td>{entry.submitted ? 'Yes' : 'No'}</td>
-            <td>
-              {convert(entry.time).minutes.toString().length === 1 && 0}
-              {convert(entry.time).minutes}:
-              {convert(entry.time).seconds.toString().length === 1 && 0}
-              {convert(entry.time).seconds} mins
-            </td>
-            <td>{entry.score}</td>
           </tr>
         ))}
       </table>
